54c288f9cd2f449ab46d92da08a4ec4ab58c90c3
SrVladyslav <vlama@inf.upv.es>
2021-01-16 18:46:23 +0100

    Adding Hamiltonian Cycle

--- python/hamiltonian_cycle.py
+++ python/hamiltonian_cycle.py
@@ -6,7 +6,7 @@ class HamiltonianCycle:
         self.E = E
         self.G = Graph(E= self.E)
         
-    def get_cycle(self):
+    def solve(self):
         def backtracking(path):
             if len(path) == len(self.G.V):
                 if (path[-1], path[0]) in G.E:           # The end is the starting point of a Graph
@@ -26,3 +26,5 @@ if __name__ == '__main__':
     hc = HamiltonianCycle(
         E = [(0,1), (0,2), (0,3), (1,3), (1,4), (2,3), (2,5), (3,4), (3,5), (3,6), (4,7), (5,6), (5,8), (6,7), (6,8), (6,9) (7,9), (8,9)]
     )
+    
+    hc.solve()
